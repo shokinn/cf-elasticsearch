74ce638799f88f3853d8de3e801b4f0e47f7b081
Salman Malik <smalik@pivotal.io>
2018-11-12 12:18:07 -0500

    Add setup.sh to download the JDK and ES tarballs. The start.sh script untar in the container at runtime

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf *.tar.gz
+
+# download JDK
+wget https://cdn.azul.com/zulu/bin/zulu8.33.0.1-jdk8.0.192-linux_x64.tar.gz
+
+# download elasticsearch
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.3.tar.gz
--- start.sh
+++ start.sh
@@ -5,7 +5,10 @@ mkdir data
 
 export LOG_DIR=$PWD/log
 export DATA_DIR=$PWD/data
-export JAVA_HOME=$PWD/jre1.8.0_191
-export ES_HOME=$PWD/elasticsearch-6.4.3
+
+for tarfile in `ls *.tar.gz`; do tar xzf $tarfile; rm $tarfile; done
+
+export JAVA_HOME=$PWD/$(ls | grep jdk)
+export ES_HOME=$PWD/$(ls | grep elasticsearch)
 
 $ES_HOME/bin/elasticsearch -E 'http.port=8080' -E 'network.host=0.0.0.0' -E 'discovery.type=single-node' 
